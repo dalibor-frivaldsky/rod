22304c30f7126a69f22fb50005111c9a707aa717
Dalibor Frívaldský <dalibor.free@gmail.com>
2016-12-29 13:06:18 +0100

    Added test showing the primary problem with constexpr resoling

--- test/unit/rod/resolve.spec.cpp
+++ test/unit/rod/resolve.spec.cpp
@@ -25,3 +25,15 @@ SCENARIO( "rod::resolve evaluates resolve_impl function defined in Source type's
 	}
 
 }
+
+SCENARIO( "rod::resolve evaluates in constexpr context if the resolve_impl is constexpr", "[unit]" ) {
+
+	GIVEN( "resolve_impl function" ) {
+
+		THEN( "rod::resolve can be constexpr-evaluated with expected arguments" ) {
+			static_assert( test::resolve_impl(test::Foo{}, 2) == 5 );
+			//static_assert( 5 == rod::resolve(2)(test::Foo{}) );
+		}
+	}
+
+}
